f14850861e2e8c9c5f4feb97ba2f5241350c7a7f
kenneth cheng <kenny@kenneths-MacBook-Pro.local>
2021-10-27 22:42:38 -0400

    Added functionality to the sign up form

--- client/src/components/signInForm/index.js
+++ client/src/components/signInForm/index.js
@@ -1,8 +1,29 @@
-const Form = () => {
+import React, { useState } from 'react';
+
+const Form = (props) => {
+  const [signupInfo, setSignUp] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [allSignUpInfo, setAllSignUp] = useState([]);
+
+  const handleInput = (info, value) => {
+    setSignUp({ ...signupInfo, [info]: value })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const updatedForms = [...allSignUpInfo, signupInfo]
+    setAllSignUp(updatedForms);
+    setSignUp({ email: '', password: '' });
+    console.log(signupInfo);
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Email"/>
-      <input type="text" placeholder="Password"/>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Email" value={signupInfo.email} onChange={e => handleInput('email', e.target.value)} />
+      <input type="text" placeholder="Password" value={signupInfo.password} onChange={e => handleInput('password', e.target.value)} />
       <input type="submit" value="submit" />
     </form>
   );
